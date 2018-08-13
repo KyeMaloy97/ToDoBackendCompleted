ab3606594d81bb508d321232f1b89f6b6d58046b
Kye Maloy <kye.maloy@ibm.com>
2018-08-13 16:51:41 +0100

    fixed model extension location

--- ToDoServer/Sources/Application/Application.swift
+++ ToDoServer/Sources/Application/Application.swift
@@ -114,6 +114,10 @@ public class App {
 	}
 }
 
+extension ToDo: Model {
+}
+
+
 class Persistence {
 	static func setUp() {
 		let pool = PostgreSQLConnection.createPool(host: "localhost", port: 5432, options: [.databaseName("tododb")], poolOptions: ConnectionPoolOptions(initialCapacity: 10, maxCapacity: 50, timeout: 10000))
--- ToDoServer/Sources/Application/Model.swift
+++ ToDoServer/Sources/Application/Model.swift
@@ -19,7 +19,3 @@ public struct ToDo : Codable, Equatable {
 		return (lhs.title == rhs.title) && (lhs.user == rhs.user) && (lhs.order == rhs.order) && (lhs.completed == rhs.completed) && (lhs.url == rhs.url) && (lhs.id == rhs.id)
 	}
 }
-
-extension ToDo: Model {
-}
-
